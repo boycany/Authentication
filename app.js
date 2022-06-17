4c48060467e957c4e832a22b0479fc8060291818
111084 <111084@w.tmu.edu.tw>
2022-06-17 16:00:50 +0800

    ch30-4 done: basic login and signup

---
+++ app.js
@@ -0,0 +1,108 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const { request } = require("http");
+const session = require("express-session");
+const flash = require("connect-flash");
+
+//mongodb models
+const User = require("./models/user");
+
+app.set("view engine", "ejs");
+app.use(cookieParser(process.env.SECRET));
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+    // cookie: { secure: true },
+  })
+);
+app.use(flash());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose
+  .connect("mongodb://localhost:27017/test", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to mongodb.");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+app.get("/", (req, res) => {
+  req.flash("success_msg", "Successfully get to the homepage.");
+  res.send("Hi, " + req.flash("success_msg"));
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/login", async (req, res, next) => {
+  let { username, password } = req.body;
+  try {
+    let foundUser = await User.findOne({ username });
+    if (foundUser && password === foundUser.password) {
+      res.render("secret");
+    } else {
+      res.send("Username or Passowrd not correct.");
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+app.post("/signup", (req, res, next) => {
+  console.log(req.body);
+  let { username, password } = req.body;
+  let newUser = new User({ username, password });
+  try {
+    newUser
+      .save()
+      .then(() => {
+        res.send("Data has been saved.");
+      })
+      .catch((e) => {
+        res.send("Error.");
+      });
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get("/verifyUser", (req, res) => {
+  req.session.isVerified = true;
+  res.send("You are verified.");
+});
+
+app.get("/secret", (req, res) => {
+  if (req.session.isVerified) {
+    res.send("mySecret is HERE!");
+  } else {
+    res.status(403).send("You are not authorized to see my secret.");
+  }
+});
+
+app.get("/*", (req, res) => {
+  res.status(404).send("404 Page Not Found");
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).send("Something is broken.");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
